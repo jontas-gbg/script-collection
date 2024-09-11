132e43e117ef11c923b66c74c7da281485f82d75
Jonas Stolt <jontas@gmx.com>
2024-09-11 10:09:55 +0200

    Using Zenity for dialog box

--- set_timer.sh
+++ set_timer.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# A simple timer called from an alias named 'st' like "st 3m" 
+# or "st 1h"
+# jontas@gmx.com in 2022, updated in 2024 using Zenity for dialog box.
+
 if [ $# -eq 0 ]; then
     zenity --error --text="Specify countdown time.\nSomething like:\n     st 3m\nto set a timer for 3 minutes"
     exit 1
