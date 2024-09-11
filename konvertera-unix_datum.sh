2867103aa2ca007bb08cca9ef85d933ee79ca015
Jonas Stolt <jontas@gmx.com>
2024-09-11 09:57:42 +0200

    Change variables to fit Google bash style guide

--- konvertera-unix_datum.sh
+++ konvertera-unix_datum.sh
@@ -3,22 +3,22 @@
 # Convert Epoch timestamps to human readable.
 # jontas@gmx.com in 2017
 
-input_file="$HOME/.xsession-errors"
-output_file="$HOME/.local/share/mylogs/xsession-errors.log"
+INPUT_FILE="$HOME/.xsession-errors"
+OUTPUT_FILE="$HOME/.local/share/mylogs/xsession-errors.log"
 
 # Loopa igenom varje rad i .xsession-errors
 while read -r line; do
     # Hitta Unix-tidsstämplar i raden (om de finns)
-    timestamps=$(echo "$line" | grep -oE '[0-9]{10}')
+    timestamps=$(echo "${line}" | grep -oE '[0-9]{10}')
 
     # Konvertera varje Unix-tidsstämpel till läsbar form
-    for timestamp in $timestamps; do
-        readable_date=$(date -d "@$timestamp" "+%Y-%m-%d %H:%M:%S")
-        line=$(echo "$line" | sed "s/$timestamp/$readable_date/")
+    for timestamp in ${timestamps}; do
+        readable_date=$(date -d "@${timestamp}" "+%Y-%m-%d %H:%M:%S")
+        line=${line//${timestamp}/${readable_date}}
     done
 
     # Skriv den uppdaterade raden till den nya filen
-    echo "$line" >> "$output_file"
-done < "$input_file"
+    echo "${line}" >> "${OUTPUT_FILE}"
+done < "${INPUT_FILE}"
 
-echo "Konverterade felmeddelanden sparade i $output_file"
+echo "Konverterade felmeddelanden sparade i ${OUTPUT_FILE}"
