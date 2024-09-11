f0734e50a4a8128b32089dd6620141b774a235d8
Jonas Stolt <jontas@gmx.com>
2024-09-11 10:05:10 +0200

    Updated dialog box to use Zenity

--- pomodoro_timer.sh
+++ pomodoro_timer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Don't sit and stare on the computer screen too long.
-# jontas@gmx.com
+# Don't sit and stare at the computer screen too long.
+# jontas@gmx.com in 2022
 
 (sleep 25m && zenity --info --text="POMODORO!\nGet up on your feet") &
