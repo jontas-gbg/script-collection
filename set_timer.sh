4aad2f29978eedaef95e0f65a2fac432372cd7ab
Jonas Stolt <jontas@gmx.com>
2024-09-11 10:15:38 +0200

    Fix spelling error

--- set_timer.sh
+++ set_timer.sh
@@ -5,7 +5,7 @@
 # jontas@gmx.com in 2022, updated in 2024 using Zenity for dialog box.
 
 if [ $# -eq 0 ]; then
-    zenity --error --text="Specify countdown time.\nSomething like:\n     st 3m\nto set a timer for 3 minutes"
+    zenity --error --text="Specify countdown time."
     exit 1
 fi
 
