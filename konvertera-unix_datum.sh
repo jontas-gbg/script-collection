6c609a15fbd7a7dede3b2f5970c11240408e39a3
Jonas Stolt <jontas@gmx.com>
2024-09-11 09:59:28 +0200

    Fix indentations to follow Google bash style guide

--- konvertera-unix_datum.sh
+++ konvertera-unix_datum.sh
@@ -8,17 +8,16 @@ OUTPUT_FILE="$HOME/.local/share/mylogs/xsession-errors.log"
 
 # Loopa igenom varje rad i .xsession-errors
 while read -r line; do
-    # Hitta Unix-tidsstämplar i raden (om de finns)
-    timestamps=$(echo "${line}" | grep -oE '[0-9]{10}')
+  # Hitta Unix-tidsstämplar i raden (om de finns)
+  timestamps=$(echo "${line}" | grep -oE '[0-9]{10}')
 
-    # Konvertera varje Unix-tidsstämpel till läsbar form
-    for timestamp in ${timestamps}; do
-        readable_date=$(date -d "@${timestamp}" "+%Y-%m-%d %H:%M:%S")
-        line=${line//${timestamp}/${readable_date}}
-    done
-
-    # Skriv den uppdaterade raden till den nya filen
-    echo "${line}" >> "${OUTPUT_FILE}"
+  # Konvertera varje Unix-tidsstämpel till läsbar form
+  for timestamp in ${timestamps}; do
+    readable_date=$(date -d "@${timestamp}" "+%Y-%m-%d %H:%M:%S")
+    line=${line//${timestamp}/${readable_date}}
+  done
+  # Skriv den uppdaterade raden till den nya filen
+  echo "${line}" >> "${OUTPUT_FILE}"
 done < "${INPUT_FILE}"
 
 echo "Konverterade felmeddelanden sparade i ${OUTPUT_FILE}"
